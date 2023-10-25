8cd5adb57b423bbe0de2be683b2cccfe4679a6fb
Foundsheep <moondeokjong@gmail.com>
2023-10-25 17:16:34 +0900

    [Added] config.py to handle configs in one file

---
+++ config.py
@@ -0,0 +1,9 @@
+import torch
+
+ROOT = ""
+LEARNING_RATE = 1e-3
+BATCH_SIZE = 32
+EPOCHS = 10
+WIDTH_MULTIPLIER = 1
+RESOLUTION_MULTIPLIER = 1
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
